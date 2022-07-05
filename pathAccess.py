de4dc52eab233647f5f192a8697bd41ce98d0529
PandoraV <kuailebsnl@gmail.com>
2022-07-05 22:15:46 +0800

    跳过OUTPUT文件并固定电压电流顺序

--- pathAccess.py
+++ pathAccess.py
@@ -6,8 +6,6 @@ import os
 import re
 import numpy as np
 
-storage_flag = False
-
 path = './data'
 parent_dir = os.listdir(path)
 for child_dir in parent_dir:
@@ -15,18 +13,17 @@ for child_dir in parent_dir:
     for file_name in file_list:
         searchObj = re.search(r'(.*).CSV', file_name, re.I|re.M)
         if searchObj:
+            if (searchObj.group(0) == 'OUTPUT.CSV'):
+                continue
             # 匹配到目标数据集
             target_path = path + '/' + child_dir + '/' + searchObj.group(0)
             # print(target_path)
             with open(target_path, mode='r') as f:
                 data = np.loadtxt(f, str, delimiter=',')
-                if storage_flag is False:
+                if searchObj.group(0)[7] == '1':
                     y1 = data[:, 4]
-                    # y1.reshape(2500, 1)
-                    storage_flag = True
                 else:
                     y2 = data[:, 4]
-                    storage_flag = False
                 f.close()
     y = np.c_[y1, y2] # 要写入到文件里的数据
     storage_path = path + '/' + child_dir + '/' + "OUTPUT.CSV"
