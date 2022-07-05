a32556436241d2047f934638687ca81cfe622ccd
PandoraV <kuailebsnl@gmail.com>
2022-07-05 22:10:30 +0800

    访问电流电压并且存在目录下

--- pathAccess.py
+++ pathAccess.py
@@ -1,7 +1,12 @@
+# %%
 # coding=utf-8
 
+from nis import cat
 import os
 import re
+import numpy as np
+
+storage_flag = False
 
 path = './data'
 parent_dir = os.listdir(path)
@@ -13,3 +18,18 @@ for child_dir in parent_dir:
             # 匹配到目标数据集
             target_path = path + '/' + child_dir + '/' + searchObj.group(0)
             # print(target_path)
+            with open(target_path, mode='r') as f:
+                data = np.loadtxt(f, str, delimiter=',')
+                if storage_flag is False:
+                    y1 = data[:, 4]
+                    # y1.reshape(2500, 1)
+                    storage_flag = True
+                else:
+                    y2 = data[:, 4]
+                    storage_flag = False
+                f.close()
+    y = np.c_[y1, y2] # 要写入到文件里的数据
+    storage_path = path + '/' + child_dir + '/' + "OUTPUT.CSV"
+    np.savetxt(storage_path, y, delimiter=",", fmt="%s")
+
+# %%
