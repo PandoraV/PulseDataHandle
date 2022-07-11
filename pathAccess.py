7fc63945451dfc4f4c756e972fa86d7b04c4ad05
JD <zhangdi_jd@cau.edu.cn>
2022-07-11 20:58:42 +0800

    针对苹果存储目录的优化

--- pathAccess.py
+++ pathAccess.py
@@ -9,8 +9,12 @@ import numpy as np
 path = './data'
 parent_dir = os.listdir(path)
 for child_dir in parent_dir:
+    if child_dir == ".DS_Store":
+        continue
     file_list = os.listdir(path + '/' + child_dir)
     for file_name in file_list:
+        if file_name == ".DS_Store":
+            continue
         searchObj = re.search(r'(.*).CSV', file_name, re.I|re.M)
         if searchObj:
             if (searchObj.group(0) == 'OUTPUT.CSV'):
