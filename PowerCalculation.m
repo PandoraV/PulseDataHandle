34de6b0c0d28c2588b23f46765ff342cd3f9d204
JD <zhangdi_jd@cau.edu.cn>
2022-08-19 16:57:22 +0800

    update .gitignore

--- PowerCalculation.m
+++ PowerCalculation.m
@@ -9,13 +9,13 @@ t = linspace(0,2500,2500);
 U = smooth(U_origin,30,'rlowess');
 I = smooth(I_origin,30,'rlowess');  		% 利用rlowess方法对y进行平滑处理
 
-% figure;  							        % 新建一个图形窗口
-% plot(t,U_origin,'k:');  			        % 绘制加噪波形图
-% hold on;      
-% plot(t,U,'k','linewidth',3);  	        % 绘制平滑后波形图
-% xlabel('t');  					        % 为X轴加标签
-% ylabel('rlowess');  				        % 为Y轴加标签
-% legend('加噪波形','平滑后波形');
+figure;  							        % 新建一个图形窗口
+plot(t,I_origin,'k:');  			        % 绘制加噪波形图
+hold on;      
+plot(t,I,'k','linewidth',3);  	        % 绘制平滑后波形图
+xlabel('t');  					        % 为X轴加标签
+ylabel('rlowess');  				        % 为Y轴加标签
+legend('加噪波形','平滑后波形');
 
 power_origin = zeros(2500, 1);
 power_handled = zeros(2500, 1);
